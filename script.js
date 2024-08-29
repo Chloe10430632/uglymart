fd09e2e05abff9c99dab09f8461be6d7ca5b35aa
Chloe10430632 <111947982+Chloe10430632@users.noreply.github.com>
2024-08-29 14:30:56 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,353 @@
+// 首頁商品資料陣列-----------------
+const home=[
+  {
+    name:"BT Blender Bottle 訂製款",
+    price: 1080,
+    url1:"https://shoplineimg.com/6129efefcb5f400016514425/651fe20613ccbd001dba7705/800x.webp?source_format=jpg",
+    url2:"https://shoplineimg.com/6129efefcb5f400016514425/651fe206c5d3e0000e25a71d/375x.webp?source_format=jpg",
+    num:0
+  },
+  
+  {
+    name:"KINGJUN BT/蠢兔 金光雙層手機殼",
+    price: 880,
+    url1:"https://shoplineimg.com/6129efefcb5f400016514425/65276ecc4eefee000e7af98e/800x.webp?source_format=jpg",
+    url2:"https://shoplineimg.com/6129efefcb5f400016514425/652e53bb94ad83001d880072/800x.webp?source_format=jpg",
+    num:0
+  },
+  
+  {
+    name:"KINGJUN大象 金屬鑰匙圈",
+    price: 250,
+    url1:"https://shoplineimg.com/6129efefcb5f400016514425/65af8f99b62b91001137e418/800x.webp?source_format=jpg",
+    url2:"https://shoplineimg.com/6129efefcb5f400016514425/65af8f9970181900203137e6/800x.webp?source_format=jpg",
+    num:0
+  },
+  
+  {
+    name:"雙層真空不鏽鋼杯 BT/蠢兔",
+    price: 680,
+    url1:"https://shoplineimg.com/6129efefcb5f400016514425/65277aa3d37b08001a2c26c7/800x.webp?source_format=jpg",
+    url2:"https://shoplineimg.com/6129efefcb5f400016514425/65277aa4b41f1a001404ea28/800x.webp?source_format=jpg",
+    num:0
+  },
+  
+  {
+    name:"KINGJUN滴膠壓克力氣囊支架 (4款)",
+    price: 250,
+    url1:"https://shoplineimg.com/6129efefcb5f400016514425/647c062075a2b0002024da65/800x.webp?source_format=jpg",
+    url2:"https://shoplineimg.com/6129efefcb5f400016514425/647c068a91595000145dabe5/800x.webp?source_format=jpg",
+    num:0
+  },
+  
+  {
+    name:"KINGJUN 刺繡厚磅Ｔ 大象/BT款",
+    price: 1180,
+    url1:"https://shoplineimg.com/6129efefcb5f400016514425/651ffe0db56a3c00204be160/800x.webp?source_format=jpg",
+    url2:"https://shoplineimg.com/6129efefcb5f400016514425/651ffe0ef05e540023177171/800x.webp?source_format=jpg",
+    num:0
+  },
+  
+  {
+    name:"KINGJUN Socks 經典Logo刺繡襪",
+    price: 250,
+    url1:"https://shoplineimg.com/6129efefcb5f400016514425/647d5bc4eac84f56b2454110/800x.webp?source_format=jpg",
+    url2:"https://shoplineimg.com/6129efefcb5f400016514425/647d69daf661cd00118b716d/800x.webp?source_format=jpg",
+    num:0
+  },
+];
+
+// CLOTHE商品資料陣列----------------
+const clothe=[
+  {
+    name:"KINGJUN 刺繡厚磅Ｔ 大象/BT款",
+    price: 1180,
+    url1:"https://shoplineimg.com/6129efefcb5f400016514425/651ffe0db56a3c00204be160/800x.webp?source_format=jpg",
+    url2:"https://shoplineimg.com/6129efefcb5f400016514425/651ffe0ef05e540023177171/800x.webp?source_format=jpg",
+    num:0
+  },
+    
+  {
+    name:"KINGJUN 展覽限定 花花短Tee",
+    price: 1080,
+    url1:"https://shoplineimg.com/6129efefcb5f400016514425/647abaf8331dc1002353a410/800x.webp?source_format=png",
+    url2:"https://shoplineimg.com/6129efefcb5f400016514425/647abb0a5e073f0014c6e2d7/800x.webp?source_format=png",
+    num:0 
+  },
+    
+  {
+    name:"KINGJUN Socks 經典Logo刺繡襪",
+    price: 250,
+    url1:"https://shoplineimg.com/6129efefcb5f400016514425/647d5bc4eac84f56b2454110/800x.webp?source_format=jpg",
+    url2:"https://shoplineimg.com/6129efefcb5f400016514425/647d69daf661cd00118b716d/800x.webp?source_format=jpg",
+    num:0
+  },
+];
+
+// BAG商品資料陣列----------------
+const bag=[
+  {
+    name:"KINGJUN 大象 隨身厚磅提袋 灰/粉",
+    price: 850,
+    url1:"https://shoplineimg.com/6129efefcb5f400016514425/652516bd0327e763937df9df/800x.webp?source_format=jpg",
+    url2:"https://shoplineimg.com/6129efefcb5f400016514425/652516bc5ba551001a83d43d/800x.webp?source_format=jpg",
+    num:0
+  },
+  {
+    name:"KINGJUN 跑車提袋",
+    price: 980,
+    url1:"https://shoplineimg.com/6129efefcb5f400016514425/647acc52642ce3001a420895/800x.webp?source_format=png",
+    url2:"https://shoplineimg.com/6129efefcb5f400016514425/647ac412e0575d001a127d0c/800x.webp?source_format=png",
+    num:0
+  },
+  {
+    name:"KINGJUN 病毒刺繡斜掛包 灰/紫",
+    price: 850,
+    url1:"https://shoplineimg.com/6129efefcb5f400016514425/652527c4d3f69100231bd5c2/800x.webp?source_format=jpg",
+    url2:"https://shoplineimg.com/6129efefcb5f400016514425/652527c40563ef001d333adf/800x.webp?source_format=jpg",
+    num:0
+  },
+];
+
+// ACC商品資料陣列----------------
+const acc=[
+  {
+    name:"心情符號 KINGMOJI眼罩",
+    price: 280,
+    url1:"https://shoplineimg.com/6129efefcb5f400016514425/65290b50e05cb70020515bc5/800x.webp?source_format=jpeg",
+    url2:"https://shoplineimg.com/6129efefcb5f400016514425/65290b502328c3000e1ecb5d/800x.webp?source_format=jpeg",
+    num:0
+  },
+  {
+    name:"Kingmoji 心情符號 Airpods 雙開耳機殼",
+    price: 450,
+    url1:"https://shoplineimg.com/6129efefcb5f400016514425/647f6014f9c6c80017fb57d2/800x.webp?source_format=png",
+    url2:"https://shoplineimg.com/6129efefcb5f400016514425/647f6014c056c000207feb10/800x.webp?source_format=png",
+    num:0
+  },
+  {
+    name:"KINGJUN 跑車法蘭絨毛毯",
+    price: 850,
+    url1:"https://shoplineimg.com/6129efefcb5f400016514425/647dba0ce50f3c000eb5ee26/800x.webp?source_format=jpg",
+    url2:"https://shoplineimg.com/6129efefcb5f400016514425/647db9c5a1ff3e0020fd1ec9/800x.webp?source_format=jpg",
+    num:0
+  },
+];
+
+// 購物車資料，全域變數空陣列-------------
+window.list = [];
+
+// html模板----------
+var total_tem="<div id='total'><div class='tname'>Total：</div><div class='tprice'>{{price}}</div></div>"
+
+// 陣列更新後即時自動渲染
+const { createApp, ref } = Vue;
+// Vue3 偵測範圍---------
+const vm=Vue.createApp({
+  data(){
+    return{
+      nav_home: true,
+      nav_clothe: false,
+      nav_bag: false,
+      nav_acc: false,
+      home: home,
+      clothe: clothe,
+      bag: bag,
+      acc: acc,
+      selectlist: ref([]),
+      activeSection: null,
+      visibleSection: null
+    }
+  },
+  methods:{
+    addhome (id) {
+      var homeName= home[id].name;
+      var CheckAllName= this.selectlist.find(item=>item.name===homeName);
+  // 如果品名相同，就在數量直接加1
+      if(CheckAllName){
+      CheckAllName.num+=1;
+      }else{ this.selectlist.push({name: home[id].name,price: home[id].price,url1: home[id].url1,num: home[id].num+1});}
+      $(".reddot").css("display","block");
+      $("#homeadd"+id).text("已加入購物車！")
+      if(list.length===0){
+        $("#selectlist").addClass("emptycart");
+        $("#total").css("display","none");
+    }else{ 
+        $("#selectlist").removeClass("emptycart");
+        $("#total").css("display","block");
+        $("#total").html("");
+        const sum=list.reduce(
+        (sum,b)=>{return sum+b.price*b.num},0)
+        console.log(sum)
+        var current_total_html=
+            total_tem.replace("{{price}}",sum);
+        $("#total").append(current_total_html);
+      }
+    },
+    addclothe (id) {
+      var clotheName= clothe[id].name;
+      var CheckAllName= this.selectlist.find(item=>item.name===clotheName);
+  // 如果品名相同，就在數量直接加1
+      if(CheckAllName){
+      CheckAllName.num+=1;
+      }else{ this.selectlist.push({name: clothe[id].name,price: clothe[id].price,url1: clothe[id].url1,num: clothe[id].num+1});}
+      $(".reddot").css("display","block");
+      $("#clotheadd"+id).text("已加入購物車！")
+      if(this.selectlist.length==0){
+        $("#selectlist").addClass("emptycart");
+        $("#total").css("display","none");
+    }else{ 
+        $("#selectlist").removeClass("emptycart");
+        $("#total").css("display","block");
+        $("#total").html("");
+        const sum=this.selectlist.reduce(
+        (sum,b)=>{return sum+b.price*b.num},0)
+        console.log(sum)
+        var current_total_html=
+            total_tem.replace("{{price}}",sum);
+        $("#total").append(current_total_html);
+      }
+    },
+    addbag (id) {
+      var bagName= bag[id].name;
+      var CheckAllName= this.selectlist.find(item=>item.name===bagName);
+  // 如果品名相同，就在數量直接加1
+      if(CheckAllName){
+      CheckAllName.num+=1;
+      }else{ this.selectlist.push({name: bag[id].name,price: bag[id].price,url1: bag[id].url1,num: bag[id].num+1});}
+      $(".reddot").css("display","block");
+      $("#bagadd"+id).text("已加入購物車！")
+      if(this.selectlist.length==0){
+        $("#selectlist").addClass("emptycart");
+        $("#total").css("display","none");
+    }else{ 
+        $("#selectlist").removeClass("emptycart");
+        $("#total").css("display","block");
+        $("#total").html("");
+        const sum=this.selectlist.reduce(
+        (sum,b)=>{return sum+b.price*b.num},0)
+        console.log(sum)
+        var current_total_html=
+            total_tem.replace("{{price}}",sum);
+        $("#total").append(current_total_html);
+      }
+    },
+    addacc (id) {
+      var accName= acc[id].name;
+      var CheckAllName= this.selectlist.find(item=>item.name===accName);
+  // 如果品名相同，就在數量直接加1
+      if(CheckAllName){
+      CheckAllName.num+=1;
+      }else{ this.selectlist.push({name: acc[id].name,price: acc[id].price,url1: acc[id].url1,num: acc[id].num+1});}
+      $(".reddot").css("display","block");
+      $("#accadd"+id).text("已加入購物車！")
+      if(this.selectlist.length==0){
+        $("#selectlist").addClass("emptycart");
+        $("#total").css("display","none");
+    }else{ 
+        $("#selectlist").removeClass("emptycart");
+        $("#total").css("display","block");
+        $("#total").html("");
+        const sum=this.selectlist.reduce(
+        (sum,b)=>{return sum+b.price*b.num},0)
+        console.log(sum)
+        var current_total_html=
+            total_tem.replace("{{price}}",sum);
+        $("#total").append(current_total_html);
+      }
+    },
+    remove_item (id) {
+      this.selectlist.splice(id,1);
+      $("#total").html("");
+      const sum=this.selectlist.reduce(
+      (sum,b)=>{return sum+b.price*b.num},0)
+      console.log(sum)
+      var current_total_html=
+          total_tem.replace("{{price}}",sum);
+      $("#total").append(current_total_html);
+      if(this.selectlist.length==0){
+        $("#selectlist").addClass("emptycart");
+        $("#total").css("display","none");}
+    },
+    minus (id) {
+      var index=$(".minus").index("#minusid"+id);
+      this.selectlist[id].num-=1;
+      index = 0;
+      if(this.selectlist[id].num==0){
+      this.selectlist.splice(id,1)}
+      $("#total").html("");
+      const sum=this.selectlist.reduce(
+      (sum,b)=>{return sum+b.price*b.num},0)
+      console.log(sum)
+      var current_total_html=
+          total_tem.replace("{{price}}",sum);
+      $("#total").append(current_total_html);
+      if(this.selectlist.length==0){
+        $("#selectlist").addClass("emptycart");
+        $("#total").css("display","none");}
+    },
+    plus (id) {
+      var index=$(".plus").index("#plusid"+id);
+      this.selectlist[id].num+=1;
+      index = 0;
+      $("#total").html("");
+      const sum=this.selectlist.reduce(
+      (sum,b)=>{return sum+b.price*b.num},0)
+      console.log(sum)
+      var current_total_html=
+          total_tem.replace("{{price}}",sum);
+      $("#total").append(current_total_html);
+    },
+    removeline (){
+      $(".clothe").removeClass("bottom_line")
+      $(".bag").removeClass("bottom_line")
+      $(".acc").removeClass("bottom_line")
+    },
+    cartblock(){
+      $(".greyscreen").css("display","block");
+      $(".cartnote").css("display","block");
+      $(".reddot").css("display","none");
+    },
+    cartnone (){
+      $(".greyscreen").css("display","none");
+      $(".cartnote").css("display","none");
+    },
+    togglebtn (){
+      this.isVisible = !this.isVisible
+      $(".greyscreen").css("display","block");
+      $(".reddot").css("display","none");
+      // $(".cartnote").css("display","block");
+    },
+    toggleSection(section) {
+      $(".greyscreen").css("display","block");
+      $(".reddot").css("display","none");
+      if (this.visibleSection === section) {
+        this.visibleSection = null;  // 隐藏内容
+      } else {
+        this.visibleSection = section; // 显示内容
+        this.activeSection = section;  // 更新当前激活的按钮
+      }},
+  },
+  computed:{
+    total: function(){
+      // $("#total").html("");
+      const sum=list.reduce(
+      (sum,b)=>{return sum+b.price*b.num},0)
+    console.log(sum)
+    }
+  },
+  mounted(){
+    this.selectlist=window.list
+    console.log(this.selectlist);
+  },
+}).mount("#ugm");
+
+
+// 滑鼠往下，Back2top會出現
+$(window).scroll(
+  function(evt){
+    if($(window).scrollTop()>0){
+      $(".back2top").css("display","block")
+    }else{
+      $(".back2top").css("display","none")
+    }});
